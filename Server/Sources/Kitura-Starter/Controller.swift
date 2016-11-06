0890dc1f0420787c10e40d37067e4e5c7129e89c
tiagomartinho <tiago.martinho@xpeppers.com>
2016-11-06 19:47:21 +0100

    exposes json on Server and reads it in the iOS app

--- Server/Sources/Kitura-Starter/Controller.swift
+++ Server/Sources/Kitura-Starter/Controller.swift
@@ -1,19 +1,3 @@
-/**
-* Copyright IBM Corporation 2016
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-**/
-
 import Kitura
 import SwiftyJSON
 import LoggerAPI
@@ -34,49 +18,15 @@ public class Controller {
 
   init() throws {
     appEnv = try CloudFoundryEnv.getAppEnv()
-
-    // All web apps need a Router instance to define routes
     router = Router()
-
-    // Serve static content from "public"
-    router.all("/", middleware: StaticFileServer())
-
-    // Basic GET request
-    router.get("/hello", handler: getHello)
-
-    // Basic POST request
-    router.post("/hello", handler: postHello)
-
-    // JSON Get request
     router.get("/json", handler: getJSON)
-  }
-
-  public func getHello(request: RouterRequest, response: RouterResponse, next: @escaping () -> Void) throws {
-    Log.debug("GET - /hello route handler...")
-    response.headers["Content-Type"] = "text/plain; charset=utf-8"
-    try response.status(.OK).send("Hello from Kitura-Starter!").end()
-  }
-
-  public func postHello(request: RouterRequest, response: RouterResponse, next: @escaping () -> Void) throws {
-    Log.debug("POST - /hello route handler...")
-    response.headers["Content-Type"] = "text/plain; charset=utf-8"
-    if let name = try request.readString() {
-      try response.status(.OK).send("Hello \(name), from Kitura-Starter!").end()
-    } else {
-      try response.status(.OK).send("Kitura-Starter received a POST request!").end()
-    }
   }
 
   public func getJSON(request: RouterRequest, response: RouterResponse, next: @escaping () -> Void) throws {
     Log.debug("GET - /json route handler...")
     response.headers["Content-Type"] = "application/json; charset=utf-8"
     var jsonResponse = JSON([:])
-    jsonResponse["framework"].stringValue = "Kitura"
-    jsonResponse["applicationName"].stringValue = "Kitura-Starter"
-    jsonResponse["company"].stringValue = "IBM"
-    jsonResponse["organization"].stringValue = "Swift @ IBM"
-    jsonResponse["location"].stringValue = "Austin, Texas"
+    jsonResponse["hello"].stringValue = "world"
     try response.status(.OK).send(json: jsonResponse).end()
   }
-
 }
--- iOS/iOS/ViewController.swift
+++ iOS/iOS/ViewController.swift
@@ -1,25 +1,15 @@
-//
-//  ViewController.swift
-//  iOS
-//
-//  Created by Tiago Martinho on 06/11/2016.
-//  Copyright © 2016 elit. All rights reserved.
-//
-
 import UIKit
+import SwiftyJSON
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        Alamofire.request("http://localhost:8090/json").responseJSON { response in
+            if let JSON = response.result.value {
+                print("JSON: \(JSON)")
+            }
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
